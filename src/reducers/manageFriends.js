a0cba2f57b41383a93ed0f8a5dbf73dc9845e9d8
McCray <tubach75@hotmail.com>
2021-09-02 11:51:40 -0400

    commit tree 941172fc4596cda0483646ac310ced4906fb963d

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,57 @@
+/*let state = {friends: []}
+
+let action = {
+    type: "ADD_FRIEND",
+    friend: {
+      name: "Chrome Boi",
+      hometown: "NYC",
+      id: 1,
+    },
+  };
+
+ let action = {
+    type: "REMOVE_FRIEND",
+    id: 1,
+  };
+
 export function manageFriends(state, action){
+    switch(action.type){
+
+    }
 }
+
+manageFriends(state, {type: "ADD_FRIEND"})
+manageFriends(state, {type: "REMOVE_FRIEND"})*/
+
+
+export function manageFriends(state = {
+  friends: [],
+}, action) {
+  switch(action.type) {
+
+    case "ADD_FRIEND":
+      return (
+        {...state,
+            friends: [
+              ...state.friends,
+              action.friend
+            ]
+        }
+    )
+
+    case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+
+    default:
+      return state;
+
+  }
+};
